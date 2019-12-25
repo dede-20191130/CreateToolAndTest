30a3f07103b8e1da4c80c046ed0cef8074f4c66f
dede_20191130 <1044adad@gmail.com>
2019-12-25 23:23:15 +0900

    20191225_2323__ Tool_Powershell_Script/hibernationTimer.ps1 | 10 ++++++++++  1 file changed, 10 insertions(+)

---
+++ Tool_Powershell_Script/hibernationTimer.ps1
@@ -0,0 +1,10 @@
+﻿Param(
+    [long]$waitTime = 300   # 待機時間（s）
+    )
+
+sleep $waitTime 
+# 休止状態
+shutdown -h
+
+# 当スクリプトを実行したコンソールウィンドウのみ閉じる（PIDは実行中のPowerShellのプロセスIDが設定される自動変数）
+Stop-Process -Id $PID
