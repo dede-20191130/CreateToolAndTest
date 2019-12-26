1c09ce4f0dde894a845b609e2f11855d3c1b9f99
dede_20191130 <1044adad@gmail.com>
2019-12-27 04:27:21 +0900

    20191227_0427__ __pycache__/test.cpython-37.pyc | Bin 0 -> 491 bytes  test.py                         |   7 ++++++-  2 files changed, 6 insertions(+), 1 deletion(-)

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
+def returnTest():
+    return "100" + "円"
+
+
 def test():
     print("aaa")
     print('Module Name: {}'.format(__name__))
+    a = returnTest()
+    print(returnTest())
 
 
 if __name__ == '__main__':
     test()
 
 print('AAA')
-
