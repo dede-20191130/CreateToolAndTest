2a7cff6c39dda080c6f4691f7b2db987619f6d08
dede_20191130 <1044adad@gmail.com>
2019-12-28 07:36:03 +0900

    20191228_0736__ .../DiceAppWithoutRandomLibrary.py                     | 18 +++++++++++++-----  1 file changed, 13 insertions(+), 5 deletions(-)

--- Tool_Python/DiceAppWithoutRandomLibrary/DiceAppWithoutRandomLibrary.py
+++ Tool_Python/DiceAppWithoutRandomLibrary/DiceAppWithoutRandomLibrary.py
@@ -2,14 +2,22 @@ import time
 import tkinter as tk
 from tkinter import messagebox
 
-if __name__ == '__main__':
+
+def diceResult():
     # 時刻によりランダムな変数取得
     temp = int((time.time_ns() % 100000000) / 100)
     # ６で割った余りを取得
-    tempRmd = temp % 6
-    if tempRmd == 0:
-        tempRmd = 6
+    myTempRmd = temp % 6
+    if myTempRmd == 0:
+        myTempRmd = 6
+
+    return myTempRmd
+
+
+if __name__ == '__main__':
+    # サイコロ結果を取得
+    result = diceResult()
     # サイコロを振った結果としてメッセージで表示
     root = tk.Tk()
     root.withdraw()  # 小さなウィンドウを表示させない
-    messagebox.showinfo("サイコロ", "出目は" + str(tempRmd) + "です")
+    messagebox.showinfo("サイコロ", "出目は" + str(result) + "です")
