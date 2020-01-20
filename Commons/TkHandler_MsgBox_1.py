9bd6897023dbb84f416eb3039555844c5350a9d7
dede_20191130 <1044adad@gmail.com>
2020-01-21 03:28:44 +0900

    20200121_0328__ Commons/TkHandler_MsgBox_1.py                      |  22 +++++++++++++++++++++  .../__pycache__/TkHandler_MsgBox_1.cpython-37.pyc  | Bin 2306 -> 2306 bytes  2 files changed, 22 insertions(+)

--- Commons/TkHandler_MsgBox_1.py
+++ Commons/TkHandler_MsgBox_1.py
@@ -44,3 +44,25 @@ class TkHandler_MsgBox_1:
             calledFunc = objMsg.normalMsgBox if not isError else objMsg.errorMsgBox
 
             calledFunc(title=myTitle, content=myContent)
+
+    # @staticmethod
+    # def showStandardMessage_AutoClose(title, content, isError=False, errorTitle=None, errorContent=None,
+    #                                   close_time=2000):
+    #     """
+    #     標準的な用法でメッセージを表示する場合に用いる。
+    #     Tkオブジェクトは再利用しない想定ですぐ解放する。
+    #     自動でメッセージを閉じる
+    #
+    #     :arg
+    #         (error)title:(エラー)タイトル
+    #         (error)content:(エラー)メッセージ内容
+    #         isError:エラーが発生した場合はエラーメッセージを表示する。
+    #         close_time:メッセージを閉じるまでの時間
+    #
+    #     """
+    #     with TkHandler_MsgBox_1() as objMsg:
+    #         myTitle = title if not isError else errorTitle
+    #         myContent = content if not isError else errorContent
+    #         calledFunc = objMsg.normalMsgBox if not isError else objMsg.errorMsgBox
+    #
+    #         objMsg.root.after(close_time, lambda: calledFunc(title=myTitle, content=myContent))
